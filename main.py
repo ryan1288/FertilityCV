d39bcd9ce5f5e8de2103b90ee59501ae3ee127a2
Ryan Lee <ryanl1288@hotmail.com>
2020-08-17 11:31:25 -0700

    Split up and re-organized files into functions Added save points for data and models Implemented watershed algorithm

---
+++ main.py
@@ -0,0 +1,99 @@
+# Author: Ryan Lee
+# Create Date: Aug. 8th, 2020
+# Purpose: Test the UNet Architecture for semantic segmentation and counting
+
+import tensorflow as tf  # Tensorflow including the Keras package within
+import os  # os to access and use directories
+import numpy as np # numpy for array operations
+
+from tensorflow.keras.models import load_model  # Load saved model
+
+# User-defined functions
+from AugmentedGenerator import create_test_arrays, create_train_arrays, create_generators  # data importer functions
+from model import create_unet  # U-Net CNN model
+from tools import pred_show, watershed_pred  # Test model prediction
+
+# Image dimensions (modify to fit microscopy)
+IMG_WIDTH = 128
+IMG_HEIGHT = 128
+IMG_CHANNELS = 3
+BATCH_SIZE = 16
+EPOCHS = 20
+VALID_SPLIT = 0.1
+
+# Dataset paths
+TRAIN_DATA_PATH = 'train/'
+TEST_DATA_PATH = 'test/'
+
+# Checkpoints to keep the best weights
+checkpoint_path = "checkpoints/test.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create checkpoints/callbacks to stop and save before overfitting
+callbacks = [
+    tf.keras.callbacks.EarlyStopping(patience=2, monitor='val_loss'),
+    tf.keras.callbacks.TensorBoard(log_dir='./logs'),
+    tf.keras.callbacks.ModelCheckpoint(checkpoint_path, save_weights_only=True, verbose=1)
+]
+
+# Initialize dataset variables
+x_test = x_train = y_train = None
+
+# Set GPU memory growth
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+
+if __name__ == '__main__':
+    # Initialize state as non-exit
+    state = 'start'
+
+    # Prompt until program is terminated
+    while state != 'exit':
+        # Select starting state
+        state = input('Select mode: (data, load_data, train, load_model, test, exit)')
+
+        if state == 'data':
+            # Use create_image_arrays() to turn the dataset into arrays
+            x_train, y_train = create_train_arrays(TRAIN_DATA_PATH, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS)
+            x_test = create_test_arrays(TEST_DATA_PATH, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS)
+            np.save('numpy_data/x_train', x_train)
+            np.save('numpy_data/y_train', y_train)
+            np.save('numpy_data/x_test', x_test)
+
+        elif state == 'load_data':
+            # Load numpy arrays
+            x_train = np.load('numpy_data/x_train.npy')
+            y_train = np.load('numpy_data/y_train.npy')
+            x_test = np.load('numpy_data/x_test.npy')
+
+        elif state == 'train':
+            # Use create_generators to make the generators for the model training
+            train_generator, val_generator = create_generators(x_train, y_train, VALID_SPLIT, BATCH_SIZE)
+
+            # Create the UNet Architecture model
+            model = create_unet(IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS)
+
+            # Using Image Generators with a 10% validation split
+            results = model.fit(train_generator, validation_data=val_generator, validation_steps=10,
+                                steps_per_epoch=250, epochs=EPOCHS, callbacks=callbacks, verbose=1)
+
+            # Save model
+            model.save("frozen_models/model.h5")
+
+        elif state == 'load_model':
+            # Load model
+            model = load_model('frozen_models/model.h5')
+
+        elif state == 'test':
+            # Select test type
+            test_type = input('Select test type: (basic, watershed)')
+
+            # Check if data is loaded
+            if x_test is None:
+                print('Data not yet loaded')
+            elif test_type == 'basic':
+                pred_show(x_test, model)
+            elif test_type == 'watershed':
+                watershed_pred(x_test, model, 0)
---
+++ tools.py
@@ -0,0 +1,93 @@
+import cv2
+import imutils
+import matplotlib.pyplot as plt  # output test plot
+import numpy as np
+import random
+
+from skimage.io import imshow  # show images as windows
+from skimage.feature import peak_local_max
+from skimage.segmentation import watershed
+from scipy import ndimage
+
+
+# Purpose: Basic mask model prediction output
+# Parameters:
+#   x_test: testing dataset
+#   model: CNN model used
+def pred_show(x_test, model):
+    # Predict random example microscopy image from test set
+    idx = random.randint(0, len(x_test))
+    x = np.array(x_test[idx])
+    x = np.expand_dims(x, axis=0)
+    predict = model.predict(x, verbose=1)
+
+    # Current prediction set to be above 50% confidence
+    predict = (predict > 0.5).astype(np.uint8)
+
+    # Show windows of predicted mask and image
+    imshow(np.squeeze(predict[0]))
+    plt.show()
+    print(np.squeeze(predict[0]).shape)
+    imshow(x_test[idx])
+    plt.show()
+
+
+# Purpose: Create data augmented generators that include the train-validation split for model training
+# Parameters:
+#   x_test: testing dataset
+#   model: CNN model used
+#   idx: index of test data to be segmented
+def watershed_pred(x_test, model, idx):
+    x_img = np.array(x_test[idx])
+    x_img_cpy = np.copy(x_img)
+    x_img_exp = np.expand_dims(x_img, axis=0)
+    predict = model.predict(x_img_exp, verbose=1)
+
+    # Current prediction set to be above 50% confidence
+    predict = (predict > 0.5).astype(np.uint8)
+
+    # Create numpy image to be used in watershed
+    image = np.squeeze(predict[0])
+
+    # Euclidian distance from background using distance_transform
+    dist = ndimage.distance_transform_edt(image)
+    local_max = peak_local_max(dist, indices=False, min_distance=6, labels=image)
+
+    # Connected component analysis before using watershed algorithm
+    markers = ndimage.label(local_max, structure=np.ones((3, 3)))[0]
+    labels = watershed(-dist, markers, mask=image)
+    print("[COUNT] {} unique instances found".format(len(np.unique(labels)) - 1))
+
+    # Loop through unique labels
+    for label in np.unique(labels):
+        # Ignore background regions
+        if label == 0:
+            continue
+
+        # Label unique detected regions
+        mask = np.zeros(image.shape, dtype="uint8")
+        mask[labels == label] = 255
+
+        # Grab largest contour in the mask
+        contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours = imutils.grab_contours(contours)
+        c = max(contours, key=cv2.contourArea)
+
+        # Draw a circle and text enclosing the detected region
+        ((x, y), r) = cv2.minEnclosingCircle(c)
+        cv2.circle(x_img_cpy, (int(x), int(y)), int(r), (0, 255, 0), 1)
+        cv2.putText(x_img_cpy, "{}".format(label), (int(x) - 10, int(y) - 2), cv2.FONT_HERSHEY_SIMPLEX, 0.3,
+                    (0, 0, 255), 1)
+
+    # Plot images for pipeline progression
+    plt.figure(1)
+    imshow(x_img)
+    plt.figure(2)
+    print(2)
+    imshow((image*255/image.max()).astype(np.uint8))
+    plt.figure(3)
+    print(3)
+    imshow((dist*255/dist.max()).astype(np.uint8))
+    plt.figure(4)
+    imshow(x_img_cpy)
+    plt.show()
